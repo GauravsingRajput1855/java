af2031c00561f5a3ec96f6da2f46b76617f4bbed
GauravsingRajput1855 <154476057+GauravsingRajput1855@users.noreply.github.com>
2024-05-28 23:59:30 +0530

    Add files via upload

---
+++ spring mvc/HelloController.java
@@ -0,0 +1,26 @@
+package com.app.controller;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	public HelloController() {
+		System.out.println("in ctor of"+getClass());
+	}
+	
+	@PostConstruct
+	public void init123()
+	{
+		System.out.println("in init"+getClass());
+	}
+	@RequestMapping("/hello")
+	public String sayHello()
+	{
+		System.out.println("in say hello");
+		return "/welcome";
+	}
+
+}
